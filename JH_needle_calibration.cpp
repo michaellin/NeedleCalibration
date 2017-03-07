b1ee5a491a7fc61f613aa41367858432f7ac0bd1
bdml <bdml@bdml-computer.(none)>
2017-03-06 22:57:01 -0800

    deleted the manually coded rotations

--- JH_needle_calibration.cpp
+++ JH_needle_calibration.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <fstream>
 
+
 #define TEST
 //#define EXPER
 
@@ -102,6 +103,45 @@ int main (int argc, char **argv)
 
 
 #ifdef TEST
+const float calibX[] {
+0.04, 0.08, 0.12,
+0.0, 0.0, 0.0,
+};
+
+const float calibY[] {
+-0.01, -0.01, -0.01,
+0.03, 0.05, 0.09,
+-0.01, -0.01, -0.01,
+};
+
+const float calibZ[] {
+-0.15, -0.15, -0.15,
+-0.15, -0.15, -0.15,
+-0.11, -0.07, -0.03,
+};
+
+const float positionX[40] {
+	-0.020, -0.060, -0.100, -0.140, -0.180,
+	0.020, 0.060, 0.100, 0.140, 0.180,
+	-0.020, -0.060, -0.100, -0.140, -0.180,
+	0.020, 0.060, 0.100, 0.140, 0.180,
+	-0.020, -0.060, -0.100, -0.140, -0.180,
+	0.020, 0.060, 0.100, 0.140, 0.180,
+	-0.020, -0.060, -0.100, -0.140, -0.180,
+	0.020, 0.060, 0.100, 0.140, 0.180,
+};
+
+const float positionZ[40] {
+	0.0, 0.0, 0.0, 0.0, 0.0,
+	0.0, 0.0, 0.0, 0.0, 0.0,
+	-0.04, -0.04, -0.04, -0.04, -0.04,
+	-0.04, -0.04, -0.04, -0.04, -0.04,
+	-0.08, -0.08, -0.08, -0.08, -0.08,
+	-0.08, -0.08, -0.08, -0.08, -0.08,
+	-0.16, -0.16, -0.16, -0.16, -0.16,
+	-0.16, -0.16, -0.16, -0.16, -0.16,
+};
+
 int main (int argc, char **argv)
 {
 
@@ -124,7 +164,7 @@ int main (int argc, char **argv)
     std::vector<tf::Transform> trajectory;
 
     //Poses
-    tf::Transform tfpose_init, tfpose_homed, tfpose_oriented;
+    tf::Transform tfpose_init, tfpose_homed, tfpose_oriented, tfpose_goal;
     tf::Transform tfpose_corner1, tfpose_corner2, tfpose_corner3, tfpose_corner4;
     tf::Transform tfpose_cwrot, tfpose_ccwrot;
     tf::Transform tfpose_temp1, tfpose_temp2, tfpose_temp3, tfpose_temp4, tfpose_temp5, tfpose_temp6, tfpose_temp7, tfpose_temp8, tfpose_temp9, tfpose_temp10;
@@ -137,7 +177,11 @@ int main (int argc, char **argv)
     bool rotated = true; // For testing purposes
     float xoffset = 0.0;
 	float yoffset = -0.1599;
-	float zoffset = -0.022;
+	float zoffset = -0.021;
+
+	//For data saving purposes
+	std::ofstream output_file;
+	int stepNum = 0;
 
     while(ros::ok())
     {
@@ -159,19 +203,18 @@ int main (int argc, char **argv)
 
 				//Rotate the end effector
 				rotateGripperZ(0.0, tool_offset, tfpose_oriented, tfpose_temp1);
-				translateGripperZ(0.0, tfpose_temp1, tfpose_temp2);
+				translateGripperZ(0.15, tfpose_temp1, tfpose_temp2);
 				translateGripperX(-0.4064, tfpose_temp2, tfpose_temp3);
-				translateGripperY(0.15, tfpose_temp3, tfpose_temp4);
-				rotateGripperY(M_PI/4, tool_offset, tfpose_temp4, tfpose_homed);
+				translateGripperY(0.37, tfpose_temp3, tfpose_homed);
 				//translateGripperX(0.009525, tfpose_prehomed, tfpose_homed);
-				trajectory.clear();
-				trajectory.push_back(tfpose_oriented);
-				trajectory.push_back(tfpose_temp1);
-				trajectory.push_back(tfpose_temp2);
-				trajectory.push_back(tfpose_temp3);
-				trajectory.push_back(tfpose_temp4);
-				trajectory.push_back(tfpose_homed);
-				robot.execute_trajectory(trajectory, 0.1, "base_link");
+		////	trajectory.clear();
+		////	trajectory.push_back(tfpose_oriented);
+		////	trajectory.push_back(tfpose_temp1);
+		////	trajectory.push_back(tfpose_temp2);
+		////	trajectory.push_back(tfpose_temp3);
+		////	trajectory.push_back(tfpose_homed);
+		////	robot.execute_trajectory(trajectory, 0.1, "base_link");
+				robot.move_to_point(tfpose_homed, "base_link", 0.1);
 			}
 			init_state = false;
 			tool_offset.setValue(xoffset, yoffset, zoffset);
@@ -182,59 +225,21 @@ int main (int argc, char **argv)
 
 		input = getchar();
 		if (input == 'c') {
-			tool_offset.setValue(xoffset, yoffset, zoffset);
-			if (rotated) {
-				rotated = false;
-				rotateGripperY(-M_PI/20, tool_offset, tfpose_homed, tfpose_temp1);
-				rotateGripperY(-M_PI/20, tool_offset, tfpose_temp1, tfpose_temp2);
-				rotateGripperY(-M_PI/20, tool_offset, tfpose_temp2, tfpose_temp3);
-				rotateGripperY(-M_PI/20, tool_offset, tfpose_temp3, tfpose_temp4);
-				rotateGripperY(-M_PI/20, tool_offset, tfpose_temp4, tfpose_temp5);
-				rotateGripperY(-M_PI/20, tool_offset, tfpose_temp5, tfpose_temp6);
-				rotateGripperY(-M_PI/20, tool_offset, tfpose_temp6, tfpose_temp7);
-				rotateGripperY(-M_PI/20, tool_offset, tfpose_temp7, tfpose_temp8);
-				rotateGripperY(-M_PI/20, tool_offset, tfpose_temp8, tfpose_temp9);
-				rotateGripperY(-M_PI/20, tool_offset, tfpose_temp9, tfpose_rotated);
-				trajectory.clear();
-				trajectory.push_back(tfpose_temp1);
-				trajectory.push_back(tfpose_temp2);
-				trajectory.push_back(tfpose_temp3);
-				trajectory.push_back(tfpose_temp4);
-				trajectory.push_back(tfpose_temp5);
-				trajectory.push_back(tfpose_temp6);
-				trajectory.push_back(tfpose_temp7);
-				trajectory.push_back(tfpose_temp8);
-				trajectory.push_back(tfpose_temp9);
-				trajectory.push_back(tfpose_rotated);
-				robot.execute_trajectory(trajectory, 0.1, "base_link");
-				ROS_INFO("rotate wrist");
-			} else {
-				rotated = true;
-				rotateGripperY(M_PI/20, tool_offset, tfpose_rotated, tfpose_temp1);
-				rotateGripperY(M_PI/20, tool_offset, tfpose_temp1, tfpose_temp2);
-				rotateGripperY(M_PI/20, tool_offset, tfpose_temp2, tfpose_temp3);
-				rotateGripperY(M_PI/20, tool_offset, tfpose_temp3, tfpose_temp4);
-				rotateGripperY(M_PI/20, tool_offset, tfpose_temp4, tfpose_temp5);
-				rotateGripperY(M_PI/20, tool_offset, tfpose_temp5, tfpose_temp6);
-				rotateGripperY(M_PI/20, tool_offset, tfpose_temp6, tfpose_temp7);
-				rotateGripperY(M_PI/20, tool_offset, tfpose_temp7, tfpose_temp8);
-				rotateGripperY(M_PI/20, tool_offset, tfpose_temp8, tfpose_temp9);
-				rotateGripperY(M_PI/20, tool_offset, tfpose_temp9, tfpose_homed);
-				trajectory.clear();
-				trajectory.push_back(tfpose_temp1);
-				trajectory.push_back(tfpose_temp2);
-				trajectory.push_back(tfpose_temp3);
-				trajectory.push_back(tfpose_temp4);
-				trajectory.push_back(tfpose_temp5);
-				trajectory.push_back(tfpose_temp6);
-				trajectory.push_back(tfpose_temp7);
-				trajectory.push_back(tfpose_temp8);
-				trajectory.push_back(tfpose_temp9);
-				trajectory.push_back(tfpose_homed);
-				robot.execute_trajectory(trajectory, 0.1, "base_link");
-				ROS_INFO("unrotate wrist");
+		} else if (input == 'n') {
+			translateGripperZ(positionZ[stepNum], tfpose_homed, tfpose_temp1);
+			translateGripperX(positionX[stepNum], tfpose_temp1, tfpose_goal);
+			robot.move_to_point(tfpose_goal, "base_link", 0.1);
+			if (stepNum < 40) {
+				stepNum++;
 			}
-		} 
+		}
+////else if (input == 'f') {
+////		string filename = sprintf("robot_pos%d", angleNumber);
+////		ROS_INFO("robot_pos%d", angleNumber);
+////		output_file.open(filename,fstream::out);
+////	} else if (input == 'w') {
+////		output_file << fixed << setprecision(5) << "hi" << "," << "hi" << endl;
+////	}
 ////	} else if (input == 'w') {
 ////		yoffset += 0.0001;
 ////		ROS_INFO("yoffset at %f", yoffset);
@@ -257,5 +262,8 @@ int main (int argc, char **argv)
 
         //ros::spinOnce();
     }
+
 }
+
+
 #endif
